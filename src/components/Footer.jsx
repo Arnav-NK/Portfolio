6065661d8c8854f1f630154d100979d444edf156
Arnav-NK <143326242+Arnav-NK@users.noreply.github.com>
2025-12-07 12:57:19 +0530

    footer images updated for vercel

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,10 +7,10 @@ const Footer = () => {
                 <div className="cpy">Copyright ©ArnavNk 2024</div>
                 <div className="icons">
                     <a href="https://www.linkedin.com/in/arnav-khond/" target="_blank" rel="noopener noreferrer">
-                        <img src="../public/assets/linkdin.png" className="i1" alt="LinkedIn" />
+                        <img src="/assets/linkdin.png" className="i1" alt="LinkedIn" />
                     </a>
                     <a href="https://github.com/Arnav-NK" target="_blank" rel="noopener noreferrer">
-                        <img src="../public/assets/github-logo.png" className="i2" alt="GitHub" />
+                        <img src="/assets/github-logo.png" className="i2" alt="GitHub" />
                     </a>
                     <a href="mailto:arnavkhond4oct@gmail.com">
                         <img src="https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/112-gmail_email_mail-512.png" className="i3" alt="Email" />
@@ -19,4 +19,5 @@ const Footer = () => {
             </footer></>
             );
 }
+
 export default Footer;
